1a59f2afb06ecabb542f58bdede5df7f71bbec69
Saurabh Singh <72705347+AnejaGitHub@users.noreply.github.com>
2025-04-16 05:49:24 +0530

    Updated Main.java

--- src/com/saurabhaneja/main/Main.java
+++ src/com/saurabhaneja/main/Main.java
@@ -6,9 +6,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-//        new Login();
-        int a =5, b = 2;
-        double d = (double) a/b;
-        System.out.println(a++ * ++b);
+       new Login();
     }
 }
